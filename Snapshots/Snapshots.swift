b92ba27eac02e4fe0719ef3cc2be9860949d2188
Ryan <github@ryan.codes>
2017-05-15 21:28:59 -0700

    Update .gitignore, fix Snapshots.swift

--- Snapshots/Snapshots.swift
+++ Snapshots/Snapshots.swift
@@ -3,26 +3,26 @@ import XCTest
 class Snapshots: XCTestCase {
     override func setUp() {
         super.setUp()
-        
+
         let app = XCUIApplication()
         setupSnapshot(app)
         app.launch()
     }
-    
+
     func testLoginView() {
         let app = XCUIApplication()
-        
+
         app.buttons["Present DropboxVC"].tap()
-        
-        // I found out the 'label' for the progres bar using The Developer tool
-        // Accessiblity Inspector.
-        
+
+        // I found out the 'label' for the progress bar using The Developer tool
+        // Accessibility Inspector.
+
         // Use "hittable" to determine if something is "hidden" or not.
         // If a view is "hittable" then isHidden is false
-        
+
         let progress = app.activityIndicators["In progress"]
         let isHidden = NSPredicate(format: "hittable == false")
-        
+
         expectation(for: isHidden, evaluatedWith: progress, handler: nil)
         waitForExpectations(timeout: 10) { (_) in
             snapshot("DropboxLoginView")
